0088145b766913fc826b9f314442707eacda2264
katrysja <catherine.slepchenko@gmail.com>
2020-09-12 22:01:44 +0300

    update some src path

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ function startGame() {
         enemy.y = -100 * setting.traffic * (i + 1);
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         enemy.style.top = enemy.y + 'px';
-        enemy.style.background = `transparent url(./image/enemy${randomEnemy}.png) center / cover no-repeat`;
+        enemy.style.background = `transparent url(./img/enemy${randomEnemy}.png) center / cover no-repeat`;
         enemy.classList.add('enemy');
 
         gameArea.appendChild(enemy);
